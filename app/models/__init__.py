61eeb71b692dbead968f8e810434c7972ed2f37d
Vinod-R123 <chinnireddyvinod123@gmail.com>
2026-07-17 11:12:28 +0530

    Delete app directory
    delete

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.core.database import Base
-from app.models.user import User, UserRole
-from app.models.project import Project, ProjectMember
-from app.models.task import Task, TaskStatus, TaskPriority
-from app.models.comment import Comment
-from app.models.activity_log import ActivityLog
